a6cf55b91de18990d3a3c6b94577a04609964166
Poyraz Uslu <48966180+boreasus@users.noreply.github.com>
2022-05-29 21:39:15 +0300

    Add files via upload

---
+++ buttonScript1.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class buttonScript1 : MonoBehaviour
+{
+
+     public void menu() {
+        SceneManager.LoadScene("mainmenu");
+    }
+      public void levels() {
+        SceneManager.LoadScene("levels");
+    }
+    public void lvl1() {
+        SceneManager.LoadScene("level1");
+    }
+
+    public void lvl2() {
+        SceneManager.LoadScene("level2");
+    }
+
+       public void lvl3() {
+        SceneManager.LoadScene("level3");
+    }
+
+       public void lvl4() {
+        SceneManager.LoadScene("level4");
+    }
+
+       public void lvl5() {
+        SceneManager.LoadScene("level5");
+    }
+
+       public void lvl6() {
+        SceneManager.LoadScene("level6");
+    }
+
+       public void lvl7() {
+        SceneManager.LoadScene("level7");
+    }
+
+       public void lvl8() {
+        SceneManager.LoadScene("level8");
+    }
+       public void lvl9() {
+        SceneManager.LoadScene("level9");
+    }
+
+   public void lvl10() {
+        SceneManager.LoadScene("level10");
+    }
+
+   public void lvl11() {
+        SceneManager.LoadScene("level11");
+    }
+
+   public void lvl12() {
+        SceneManager.LoadScene("level12");
+    }
+
+   public void lvl13() {
+        SceneManager.LoadScene("level13");
+    }
+
+   public void lvl14() {
+        SceneManager.LoadScene("level14");
+    }
+
+
+
+}
